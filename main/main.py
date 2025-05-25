49d5d14d3552cf9900cfe9ea00458056e916c2ab
sergei_bobkov_1 <bobkov@sergei.by>
2025-05-25 16:28:04 +0300

    init

--- main/main.py
+++ main/main.py
@@ -1,4 +1,4 @@
-import sys
+#import sys
 from sqlalchemy import select, update
 from fastapi import FastAPI
 import models
@@ -6,7 +6,7 @@ from schemas import RecipeIn, RecipeOut
 from database import engine, session
 
 # uvicorn main:app --reload
-sys.path.append('main/')
+#sys.path.append('main/')
 app = FastAPI()
 
 
