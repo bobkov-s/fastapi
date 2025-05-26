e4c57c302552956d6dc7d93ebc6c2a9b92da27dd
sergei_bobkov_1 <bobkov@sergei.by>
2025-05-26 23:18:15 +0300

    init

--- main/database.py
+++ main/database.py
@@ -1,4 +1,8 @@
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine
+)
 from sqlalchemy.ext.declarative import declarative_base
 
 # from sqlalchemy.orm import sessionmaker
@@ -7,5 +11,9 @@ DATABASE_URL = "sqlite+aiosqlite:///./DB_hw_26.db"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 Base = declarative_base()
-session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+session = async_sessionmaker(
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
 # session = asc_session()
